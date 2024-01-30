8bdd2572a5f8153150b574e4c3c783779a2b14c4
Владимир Александрович Райгель <vraygel@mail.ru>
2024-01-30 12:11:24 +0500

    second commit

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express');
+
+const logger = require('./middleware/logger')
+const error404 = require('./middleware/err-404')
+const indexRouter = require('./routes/index')
+const demoRouter = require('./routes/demo')
+
+
 const downloadBook = require('./routes/download_book')
 const uploadBook = require('./routes/upload_book')
 // const myMiddleware = require('./middleware/index');
 
 const app = express();
+
+app.use(logger)
+app.use('/', indexRouter)
+app.use('/public', express.static(__dirname+'/public'))
+app.use('/demo', demoRouter)
+
+app.use(error404)
 
 
 app.use('/api/books', downloadBook)
@@ -11,3 +25,13 @@ app.use('/api/books', uploadBook)
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
+
+
+
+
+
+
+
+
+
+
--- routes/upload_book.js
+++ routes/upload_book.js
@@ -4,9 +4,8 @@ const middlewareBook = require('../middleware/middleware_book')
 const idBook = require('../getid')
 
 router.post('/creature',
-    middlewareBook.single('fieldname'),
+    middlewareBook.single('fileBook'),
     (req, res) => {
-        console.log(req);
         if (req.file) {
             const { path } = req.file
             const book = {
@@ -20,10 +19,8 @@ router.post('/creature',
                 fileName: req.file.fileName,
                 fileBook: req.file.filename
             };
-            console.log(book);
             res.json({ book })
         }
-        // console.log(res.json());
         res.json()
     })
 
